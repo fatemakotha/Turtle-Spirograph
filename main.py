45a8dcc00caf1b753f9c535d09a6faf005ff3782
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-06 22:03:07 +0600

    Created a circle using circle()

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import turtle
+from turtle import Turtle,Screen
+
+kotha = Turtle()
+kotha.circle(80) #creates a circle with radius 80
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
