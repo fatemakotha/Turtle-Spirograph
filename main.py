7f9eb0163b7e5ed816257f3b81b107741c96ef4d
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-06 22:19:21 +0600

    Created a function random_color() that returns a random RGB color

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import random
+import turtle
 from turtle import Turtle,Screen
 
 kotha = Turtle()
+kotha.pencolor("blue")
+kotha.speed("fastest")
 kotha.circle(80) #creates a circle with radius 80
+turtle.colormode(255)
+
+
+def random_color(): #returns a random color
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
 
 
 
